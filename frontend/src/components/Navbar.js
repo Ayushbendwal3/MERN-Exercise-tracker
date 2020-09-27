0d283a5a1d8542946c667ef2bb8302017a0f5f75
Ayush Bendwal <bendwalayush3@gmail.com>
2020-09-27 22:08:53 +0530

    Initial Commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <Link to="/" className="navbar-brand">
         ExcerTracker
       </Link>
-      <div className="collapse navbar-collapse">
+      <div className="navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
             <Link to="/" className="nav-link">
